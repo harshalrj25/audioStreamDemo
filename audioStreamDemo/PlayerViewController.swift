dacf0f6ff0945820e7dc71e3276b367711053d34
iOSDev1 <iosdev1@iOSDev1s-MacBook-Pro.local>
2017-04-20 12:38:02 +0530

    Initial Commit

---
+++ audioStreamDemo/PlayerViewController.swift
@@ -0,0 +1,213 @@
+//
+//  PlayerViewController.swift
+//  ChurchConnect
+//
+//  Created by iOSDev1 on 27/02/17.
+//  Copyright © 2017 Harshal Jadhav. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import AVKit
+
+class PlayerViewController: UIViewController {
+    @IBOutlet weak var thumbNailImageView: UIImageView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var prevButton: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var playerSlider: UISlider!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var loadingLabel: UILabel!
+    @IBOutlet weak var seekLoadingLabel: UILabel!
+
+    var playList: NSMutableArray = NSMutableArray()
+    var timer: Timer?
+    var index: Int = Int()
+    var avPlayer: AVPlayer!
+    var isPaused: Bool!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        isPaused = false
+        playButton.setImage(UIImage(named:"pause"), for: .normal)
+        self.playList.add("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")
+        self.playList.add("https://www.hrupin.com/wp-content/uploads/mp3/testsong_20_sec.mp3")
+        self.playList.add("https://ia801409.us.archive.org/12/items/1HourThunderstorm/1HrThunderstorm.mp3")
+        self.play(url: URL(string:(playList[self.index] as! String))!)
+        self.setupTimer()
+    }
+    
+    func play(url:URL) {
+        self.avPlayer = AVPlayer(playerItem: AVPlayerItem(url: url))
+        if #available(iOS 10.0, *) {
+            self.avPlayer.automaticallyWaitsToMinimizeStalling = false
+        }
+        avPlayer!.volume = 1.0
+        avPlayer.play()
+    }
+    
+    
+    override func viewWillDisappear( _ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
+        self.avPlayer = nil
+        self.timer?.invalidate()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func playButtonClicked(_ sender: UIButton) {
+        if #available(iOS 10.0, *) {
+            self.togglePlayPause()
+        } else {
+            // showAlert "upgrade ios version to use this feature"
+           
+        }
+    }
+    
+    @available(iOS 10.0, *)
+    func togglePlayPause() {
+        if avPlayer.timeControlStatus == .playing  {
+            playButton.setImage(UIImage(named:"play"), for: .normal)
+            avPlayer.pause()
+            isPaused = true
+        } else {
+            playButton.setImage(UIImage(named:"pause"), for: .normal)
+            avPlayer.play()
+            isPaused = false
+        }
+    }
+    
+    @IBAction func nextButtonClicked(_ sender: Any) {
+        self.nextTrack()
+    }
+    
+    @IBAction func prevButtonClicked(_ sender: Any) {
+        self.prevTrack()
+    }
+    
+    @IBAction func sliderValueChange(_ sender: UISlider) {
+        let seconds : Int64 = Int64(sender.value)
+        let targetTime:CMTime = CMTimeMake(seconds, 1)
+        avPlayer!.seek(to: targetTime)
+        if(isPaused == false){
+            seekLoadingLabel.alpha = 1
+        }
+    }
+    
+    @IBAction func sliderTapped(_ sender: UILongPressGestureRecognizer) {
+        if let slider = sender.view as? UISlider {
+            if slider.isHighlighted { return }
+            let point = sender.location(in: slider)
+            let percentage = Float(point.x / slider.bounds.width)
+            let delta = percentage * (slider.maximumValue - slider.minimumValue)
+            let value = slider.minimumValue + delta
+            slider.setValue(value, animated: false)
+            let seconds : Int64 = Int64(value)
+            let targetTime:CMTime = CMTimeMake(seconds, 1)
+            avPlayer!.seek(to: targetTime)
+            if(isPaused == false){
+                seekLoadingLabel.alpha = 1
+            }
+        }
+    }
+    
+    func setupTimer(){
+        NotificationCenter.default.addObserver(self, selector: #selector(self.didPlayToEnd), name: .AVPlayerItemDidPlayToEndTime, object: nil)
+        timer = Timer(timeInterval: 0.001, target: self, selector: #selector(PlayerViewController.tick), userInfo: nil, repeats: true)
+        RunLoop.current.add(timer!, forMode: RunLoopMode.commonModes)
+    }
+    
+    func didPlayToEnd() {
+        self.nextTrack()
+    }
+    
+    func tick(){
+        if(avPlayer.currentTime().seconds == 0.0){
+            loadingLabel.alpha = 1
+        }else{
+            loadingLabel.alpha = 0
+        }
+        
+        if(isPaused == false){
+            if(avPlayer.rate == 0){
+                avPlayer.play()
+                seekLoadingLabel.alpha = 1
+            }else{
+                seekLoadingLabel.alpha = 0
+            }
+        }
+        
+        if((avPlayer.currentItem?.asset.duration) != nil){
+            let currentTime1 : CMTime = (avPlayer.currentItem?.asset.duration)!
+            let seconds1 : Float64 = CMTimeGetSeconds(currentTime1)
+            let time1 : Float = Float(seconds1)
+            playerSlider.minimumValue = 0
+            playerSlider.maximumValue = time1
+            let currentTime : CMTime = (self.avPlayer?.currentTime())!
+            let seconds : Float64 = CMTimeGetSeconds(currentTime)
+            let time : Float = Float(seconds)
+            self.playerSlider.value = time
+            timeLabel.text =  self.formatTimeFromSeconds(totalSeconds: Int32(Float(Float64(CMTimeGetSeconds((self.avPlayer?.currentItem?.asset.duration)!)))))
+            currentTimeLabel.text = self.formatTimeFromSeconds(totalSeconds: Int32(Float(Float64(CMTimeGetSeconds((self.avPlayer?.currentItem?.currentTime())!)))))
+            
+        }else{
+            playerSlider.value = 0
+            playerSlider.minimumValue = 0
+            playerSlider.maximumValue = 0
+            timeLabel.text = "Live stream \(self.formatTimeFromSeconds(totalSeconds: Int32(CMTimeGetSeconds((avPlayer.currentItem?.currentTime())!))))"
+        }
+    }
+    
+    
+    func nextTrack(){
+        if(index < playList.count-1){
+            index = index + 1
+            isPaused = false
+            playButton.setImage(UIImage(named:"pause"), for: .normal)
+            self.play(url: URL(string:(playList[self.index] as! String))!)
+          
+            
+        }else{
+            index = 0
+            isPaused = false
+            playButton.setImage(UIImage(named:"pause"), for: .normal)
+             self.play(url: URL(string:(playList[self.index] as! String))!)
+        }
+    }
+    
+    func prevTrack(){
+        if(index > 0){
+            index = index - 1
+            isPaused = false
+            playButton.setImage(UIImage(named:"pause"), for: .normal)
+             self.play(url: URL(string:(playList[self.index] as! String))!)
+            
+        }
+    }
+    
+    func formatTimeFromSeconds(totalSeconds: Int32) -> String {
+        let seconds: Int32 = totalSeconds%60
+        let minutes: Int32 = (totalSeconds/60)%60
+        let hours: Int32 = totalSeconds/3600
+        return String(format: "%02d:%02d:%02d", hours,minutes,seconds)
+    }
+    
+    @IBAction func backButtonClicked(_ sender: Any) {
+        self.dismiss(animated: true) {
+            self.avPlayer = nil
+            self.timer?.invalidate()
+        }
+    }
+    
+}
+extension AVPlayer {
+    var isPlaying: Bool {
+        return rate != 0 && error == nil
+    }
+}
+
+
